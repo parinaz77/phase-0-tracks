191966f1f9c68dec2964162cc9d2f537dbe00476
Parinaz Khosraviani <parinaz77@gmail.com>
2016-09-26 18:24:11 -0700

    added example for array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,30 @@
-array=["a","b","c","s"]
-array.each do |letter|
-  puts "Letter is #{letter}"
+##### Release 1******
+colors=["blue","orange","red","black"]
+reverse_colors=[]
+colors.each do |color|
+  reverse_colors << color.reverse
 end
+p reverse_colors
+# .map! example
+colors.map! do |color|
+  color.reverse
+end
+puts "array after map!"
+p colors
 
-myhash={"a"=>2 "b"=>3}
+## Example for Hash
+students_color={"Anna"=>"orange", "Amanda"=>"blue", "John"=>"black"}
+students_color_reverse= Hash.new
+reversed_student_color = students_color.map do |name, color|
+  color.reverse
+end
+p reversed_student_color
+# .each example
+students_color.each do |name, color|
+  students_color_reverse[name] = color.reverse
+end
+p students_color_reverse
+
+
+##### Release 2******
+
