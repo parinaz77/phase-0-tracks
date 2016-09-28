fe65cd7483eab333c07eecc86c13d3a88597ab72
Parinaz Khosraviani <parinaz77@gmail.com>
2016-09-28 11:57:15 -0700

    added puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# Puppies---------------
+# Poodle
+
+# Characteristics-------
+# Hight:10inch
+# Weight:10lb
+# Color: brown
+# tail: long
+
+# Behavior-------------
+# playful
+# bark often
+
+class Puppy
+end
+Tico = Puppy.new
+spot = Puppy.new
+spot.class
+spot == Tico
+Tico.instance_of?(Array)
+Tico.instance_of?(Puppy)
