6d02551cfb4dce4d2fc47fede4a155f301e42b7c
Parinaz Khosraviani <parinaz77@gmail.com>
2016-09-28 14:06:10 -0700

    completed 6.2 with pair

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,90 @@
+#6.2 paried with Louie Chen
 class Puppy
+  def initialize
+    p "Initializing new puppy instance..."
+  end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+    def fetch(toy)
+      puts "I brought back the #{toy}!"
+      toy
+    end
+
+    def speak(num)
+      puts "Woof!".times(num)
+  end
+  # Puppy.new.speak(3) returns "Woof!" "Woof!" "Woof!"
+
+  def roll_over
+    puts "*rolls over*"
+  end
+  # Puppy.new.roll_over returns "*rolls over*"
+
+  def dog_years(year)
+    p year*7
+  end
+  # Puppy.new.dog_years(2) returns "14"
+
+  def shake_hand
+    puts "*shakes hand*"
+  end
+  # Puppy.new.shake_hand returns "*shakes hand*"
+  Puppy.new.roll_over
+end
+class Puppy
+  def initialize
+    p "Initializing new puppy instance..."
+  end
+  # Initialize runs when we call a new puppy instance.
+  # Mispelling initialize causes it not to run.
+
+    def fetch(toy)
+      puts "I brought back the #{toy}!"
+      toy
+    end
+
+    def speak(num)
+      num.times { puts "Woof!"}
+  end
+  # Puppy.new.speak(3) returns "Woof!" "Woof!" "Woof!"
+
+  def roll_over
+    puts "*rolls over*"
+  end
+  # Puppy.new.roll_over returns "*rolls over*"
+
+  def dog_years(year)
+    p year*7
+  end
+  # Puppy.new.dog_years(2) returns "14"
+
+  def shake_hand
+    puts "*shakes hand*"
+  end
+  # Puppy.new.shake_hand returns "*shakes hand*"
+end
+
+class Kitten
+  def initialize
+    puts "Initializing new kitten instance..."
+  end
+
+  def speak(num)
+    num.times { puts "Meow!" }
+  end
+
+  def chase
+    puts "*chases toy*"
   end
 
 end
 
+kittens = []
+
+50.times do
+  kittens << Kitten.new
+end
+
+kittens.each do |kitten|
+  kitten.speak(3)
+  kitten.chase
+end
