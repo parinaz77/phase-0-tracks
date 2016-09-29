c8c62a5c372b47f45a1fa087a9a4261218b44fed
Parinaz Khosraviani <parinaz77@gmail.com>
2016-09-29 13:54:01 -0700

    completed Shout Module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,10 +1,32 @@
-module Shout  #to declare a module
-  def self.yell_angrily(words)
+# module Shout  #to declare a module
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(name)
+#     name + " is so happy "+":)"
+#   end
+# end
+# p Shout.yelling_happily("Pari")
+# p Shout.yell_angrily("Im so mad")
+
+module Shout
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
-  def self.yelling_happily(name)
+  def yelling_happily(name)
     name + " is so happy "+":)"
   end
 end
-p Shout.yelling_happily("Pari")
-p Shout.yell_angrily("Im so mad")
+
+class Angrily
+  include Shout
+end
+
+class Happily
+  include Shout
+end
+
+angrily=Angrily.new
+angrily.yell_angrily("I am so sad today")
+happily=Happily.new
+happily.yelling_happily("Anna")
