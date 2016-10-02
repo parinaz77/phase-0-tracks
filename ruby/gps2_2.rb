1587b0c8d3c88fbe842614654786d61e61b8a15d
Parinaz Khosraviani <parinaz77@gmail.com>
2016-10-02 10:37:22 -0700

    completed 6.3

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,9 +6,7 @@ def grocery(str,rmstr)
   list = "lemonade tomato onion ice-cream"
   food_a=list.split(" ")
   qty_a=qty.split(" ")
-  # # steps:
-#   # [fill in any steps here]
-#   # set default quantity
+
   i=0
   food_a.each do |item|
       grocery_list[item]=qty_a[i]
@@ -19,17 +17,10 @@ def grocery(str,rmstr)
   grocery_list=update("tomato",6,grocery_list)
   print(grocery_list)
 
-  #   # print the list to the console [can you use one of your other methods here?]
-
 end
 
-
-# # output: [what data type goes here, array or hash?]
-
 # # Method to add an item to a list
-# # input: item name and optional quantity
-# # steps:
-# # output:
+
 def add_item(str,list)
   list[str]=1
   list
@@ -37,27 +28,21 @@ end
 
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+
 def delete_item(rmstr,list)
   list.delete(rmstr)
   list
 end
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+
 def update (str,qty,list)
   list[str]=qty
   list
 end
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+
 def print(list)
   list.each do |food, qty|
     puts "#{food} with qty: #{qty}"
--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-  attr_accessor :gender
-  attr_reader :age :ethnicity
+  attr_accessor :gender, :age, :ethnicity
+  attr_reader :age, :ethnicity
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
     @gender=gender
@@ -28,10 +28,30 @@ end
 # santa.eat_milk_and_cookies("yummy")
 # santa.speak
 
-santas = []
-gender_example=["male","female"]
-ethnicity_example=["black","white"]
-gender_example.length.times do |i|
-  santas<<Santa.new(gender_example[i],ethnicity_example[i])
+# santas = []
+# gender_example=["male","female"]
+# ethnicity_example=["black","white"]
+# gender_example.length.times do |i|
+#   santas<<Santa.new(gender_example[i],ethnicity_example[i])
+# end
+# p santas
+
+# santa=Santa.new("male","white")
+# puts"age is #{santa.age} and gender is #{santa.gender}"
+# santa.gender = "black"
+# puts"now gender is #{santa.gender}"
+
+santas_array=[]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+santa=Santa.new(example_genders,example_ethnicities)
+
+10.times do
+  santa.age = rand(140)
+  santa.gender = example_genders.sample
+  santa.ethnicity = example_ethnicities.sample
+  puts "Santa gender:#{santa.gender}, age:#{santa.age}, ethnicity:#{santa.ethnicity}"
 end
+
 
