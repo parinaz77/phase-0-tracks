6fc64534f292f7720fff6ad5fc1961bdb322b571
Parinaz Khosraviani <parinaz77@gmail.com>
2016-10-09 20:55:07 -0700

    added random word generator

--- js/algos.js
+++ js/algos.js
@@ -20,11 +20,19 @@ function common_key(obj1,obj2){
 return result
 }
 
+function word_array(num){
+  var words = "abcdefghijklmnopqrstuvwxyz".split("")
+  var words_array=[]
+  for (var i=0; i<num; i++){
+    words_array.push(words[Math.floor(Math.random() * words.length)])
+  }
+return words_array
+}
+
+console.log(word_array(3))
+
 console.log(common_key({name: "Steven", age: 54},{name: "Tamir", age: 54}))
 console.log(common_key({name: "Steven", age: 54},{category: "jobs", year: 2015}))
-
-
-
 
 longest(["long phrase","longest phrase","longer phrase"])
 longest(["aa","bbb","cccc"])
