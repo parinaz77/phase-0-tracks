b1cca0822e6461081e4081f9174cf889bde58a7e
Parinaz Khosraviani <parinaz77@gmail.com>
2016-10-11 12:51:56 -0700

    added SQL commands to create table and insert data

---
+++ databases/players_table.sql
@@ -0,0 +1,22 @@
+-- CREATE TABLE players(
+--   id INTEGER PRIMARY KEY,
+--   name VARCHAR(255),
+--   player_num INT,
+--   team_id INT,
+--   FOREIGN KEY(team_id) REFERENCES players(id)
+  -- );
+
+-- INSERT INTO players (name,player_num,team_id) VALUES ("Paula Bunion",45, 1);
+-- INSERT INTO players (name,player_num,team_id) VALUES ("Grave Danger",6, 2);
+-- INSERT INTO players (name,player_num,team_id) VALUES ("April Bloodgate",12, 2);
+
+-- CREATE TABLE team(
+--   id INTEGER PRIMARY KEY,
+--   team_name VARCHAR(255)
+--   );
+
+
+-- INSERT INTO team (team_name) VALUES ("Wrecking Bellas");
+-- INSERT INTO team (team_name) VALUES ("Team Unicorn");
+
+
--- js/algos.js
+++ js/algos.js
@@ -20,19 +20,46 @@ function common_key(obj1,obj2){
 return result
 }
 
-function word_array(num){
-  var words = "abcdefghijklmnopqrstuvwxyz".split("")
-  var words_array=[]
-  for (var i=0; i<num; i++){
-    words_array.push(words[Math.floor(Math.random() * words.length)])
+
+function randWord(num){
+  var textFin="";
+  var words=[];
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+
+  for (var i =0; i<num;i++){
+    var randnum = Math.floor(Math.random()*((10 -1)))+1;
+
+    for (var t=0; t<randnum; t++){
+      var randLet = alpha[Math.floor(Math.random()*((alpha.length -1)-0))+0];
+      textFin += randLet;
+    }
+    words.push(textFin);
+    textFin="";
   }
-return words_array
+  return words
 }
 
-console.log(word_array(3))
-
+// function word_array(num){
+//   var words = "abcdefghijklmnopqrstuvwxyz".split("")
+//   var words_array=[]
+//   for (var i=0; i<num; i++){
+//     words_array.push(words[Math.floor(Math.random() * words.length)])
+//   }
+// return words_array
+// }
+console.log("--------part 1 ---------")
 console.log(common_key({name: "Steven", age: 54},{name: "Tamir", age: 54}))
 console.log(common_key({name: "Steven", age: 54},{category: "jobs", year: 2015}))
 
+console.log("--------part 2 ---------")
 longest(["long phrase","longest phrase","longer phrase"])
 longest(["aa","bbb","cccc"])
+
+console.log("--------part 3 ---------")
+console.log(randWord(3))
+
+console.log("--------part 4 ---------")
+
+
+
+
