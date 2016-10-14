8606a6835738e30d35c7bdb01adf0394f289badc
Parinaz Khosraviani <parinaz77@gmail.com>
2016-10-13 21:40:25 -0700

    created and sql file for students

---
+++ databases/solo_project/students_tables.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE students (
+--   id INTEGER PRIMARY KEY,
+--   name VARCHAR(255),
+--   age INT,
+--   graduated BOOLEAN,
+--   groups INT,
+--   FOREIGN KEY (groups) REFERENCES classGroups(id)
+-- );
+
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Jo Bi",14,"true",1);
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Bill Sandy",16,"false",2);
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Jack Ji",15,"true",1);
+
+-- CREATE TABLE classGroups(
+--   id INTEGER PRIMARY KEY,
+--   groupName VARCHAR(255)
+-- );
+
+
+-- INSERT INTO classGroups(groupName) VALUES ("Fast Learner");
+-- INSERT INTO classGroups(groupName) VALUES ("Slow Learner");
+
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Jib Kib",12,"false",1);
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Viki Somi",12,"false",2);
+-- INSERT INTO students(name,age,graduated,groups) VALUES("Jim Kivi",10,"true",1);
